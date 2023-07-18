6f7fd3fb736b13a544cdd90af2635336b41ef876
Юлия <Yulechka140200@gmail.com>
2023-07-18 17:43:19 +0500

    1

---
+++ src/AvtoController.java
@@ -0,0 +1,25 @@
+public abstract class AvtoController {
+    public String modelName;
+    public int wheelsCount;
+
+    public AvtoController(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
